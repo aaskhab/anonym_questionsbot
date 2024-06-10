9d6169f192ec055a06677a2fbea4000cc4013062
aaskhab <135864636+aaskhab@users.noreply.github.com>
2024-06-10 16:39:15 +0300

    add new db functions

--- anonim_quesbot/sql/db.py
+++ anonim_quesbot/sql/db.py
@@ -1,7 +1,9 @@
 import sqlite3
 
-
 def add_to_db(user_id, user_name, username):
+    '''
+    Добавляем пользователя в БД если его нету, если есть, то игнорируем запрос
+    '''
     with sqlite3.connect('database.sqlite') as db:
         cur = db.cursor()
 
@@ -10,6 +12,9 @@ def add_to_db(user_id, user_name, username):
 
 def get_user(user_id):
     with sqlite3.connect('database.sqlite') as db:
+        '''
+        Функция, которая делает проверку на то, есть ли пользователь в БД
+        '''
         cur = db.cursor()
 
         user = cur.execute('SELECT * FROM users WHERE user_id=?', (user_id,)).fetchone()
@@ -18,3 +23,23 @@ def get_user(user_id):
             return True
         else:
             return False
+        
+def update_user(user_id, user_name, username):
+    '''
+    Функция, которая обновляет данные пользователя из БД 
+    '''
+    with sqlite3.connect('database.sqlite') as db:
+        cur = db.cursor()
+
+        cur.execute('UPDATE users set user_name = ?, username = ? where user_id = ?', (user_name, username, user_id))
+
+def get_stat():
+    '''
+    Запрос к БД, где мы получаем всех пользователей, для дальнейшего вывода статистики
+    '''
+    with sqlite3.connect('database.sqlite') as db:
+        cur = db.cursor()
+
+        stat = cur.execute('SELECT id FROM users').fetchall()
+
+        return len(stat)
